3493162829f09b32641ad4599ad52df83c5a698d
dig04214 <dig04214@naver.com>
2025-05-05 01:30:43 +0900

    feat : 프로젝트 초기 세팅
    - spring initializr 로 프로젝트 초기 세팅

---
+++ backend/Anchor/src/test/java/com/dig04214/anchor/AnchorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dig04214.anchor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnchorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
